7479c14c02704a38ae435cec23a7841bad9a9338
Christian Parsons <chparsons@gmail.com>
2011-07-12 22:02:52 -0300

    example

---
+++ of-addon/example/src/app.cpp
@@ -0,0 +1,191 @@
+#include "app.h"
+
+void app::setup()
+{
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	
+	init_keys();
+	debug_depth_texture = false;
+	
+	if (!init_kinect())
+		return;
+	
+	camluc.setup(ofToDataPath("kinect_calibration.yml").c_str(),
+				 ofToDataPath("projector_calibration.yml").c_str(),
+				 raw_depth_pix, rgb_pix);
+	
+	ofAddListener(camluc.update_texture, this, &app::update_texture);
+	ofAddListener(camluc.render_texture, this, &app::render_texture);
+}
+
+void app::update()
+{
+	update_keys();
+	
+	if (!update_kinect())
+		return;
+	
+	if (debug_depth_texture)
+	{
+		camluc.update(raw_depth_pix, rgb_pix,
+					  kinect.getDepthTextureReference());
+	}
+	else 
+	{
+		camluc.update(raw_depth_pix, rgb_pix);
+	}
+}
+
+void app::draw()
+{	
+	if (!kinect.isConnected())
+		return;	
+	
+	if (debug_depth_texture)
+	{
+		camluc.render(kinect.getDepthTextureReference());
+	}
+	else 
+	{
+		camluc.render();
+	}
+}
+
+void app::exit()
+{
+	cout << "exit!" << endl;
+	
+	ofRemoveListener(camluc.update_texture, this, &app::update_texture);
+	ofRemoveListener(camluc.render_texture, this, &app::render_texture);
+	
+	kinect.close();
+}
+
+void app::update_texture(ofEventArgs &args)
+{
+	
+}
+
+void app::render_texture(ofEventArgs &args)
+{
+	glClearColor(0.5, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	float w = 1024;
+	float h = 768;
+	
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, w, 0, h, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	glColor3f(1, 1, 0);
+	ofCircle(200, 200, 100);
+}
+
+bool app::init_kinect()
+{
+	// TODO update_calibration only when debugging depth_texture...
+	
+	kinect.update_calibration = debug_depth_texture;
+	kinect.enableDepthNearValueWhite(false);
+	
+	kinect.init(false, true, true);
+	kinect.setVerbose(false);
+	kinect.open();
+	//	ktilt = 0;
+	//	kinect.setCameraTiltAngle(ktilt);
+	
+	return update_kinect();
+}
+
+bool app::update_kinect()
+{
+	if (!kinect.isConnected())
+		return false;
+	
+	kinect.update();
+	raw_depth_pix = kinect.getRawDepthPixels();
+	rgb_pix = kinect.getPixels();
+	
+	return true;
+}
+
+void app::keyPressed(int key)
+{
+	pressed[key] = true;
+	
+	switch (key)
+	{		
+		case 'm':
+			if (ofGetWindowPositionX() == 0)
+			{
+				ofSetWindowPosition(1440,0);
+				ofSetFullscreen(true);
+			}
+			else
+			{
+				ofSetWindowPosition(0,0);
+				ofSetFullscreen(false);
+			}
+			break;		
+						
+		case 'o':
+			debug_depth_texture = !debug_depth_texture;
+			kinect.update_calibration = debug_depth_texture;
+			break;
+	}
+	
+	if (key == 'd')
+	{
+		debug();
+	}
+}
+
+void app::keyReleased(int key)
+{
+	pressed[key] = false;
+}
+
+void app::init_keys()
+{
+	for (int i = 0; i < 512; i++) 
+		pressed[i] = false;
+}
+
+void app::update_keys()
+{
+	
+}
+
+void app::mouseMoved(int x, int y )
+{
+	
+}
+
+void app::mouseDragged(int x, int y, int button)
+{
+	
+}
+
+void app::mousePressed(int x, int y, int button)
+{
+	
+}
+
+void app::mouseReleased(int x, int y, int button)
+{
+	
+}
+
+void app::resized(int w, int h)
+{
+	
+}
+
+void app::debug()
+{
+	
+}
---
+++ of-addon/example/src/app.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+#include "CamaraLucida.h"
+
+class app : public ofBaseApp 
+{
+public:
+	
+	CamaraLucida camluc;
+		
+	void update_texture(ofEventArgs &args);
+	void render_texture(ofEventArgs &args);
+		
+	bool debug_depth_texture;
+	
+	//	kinect
+	
+	ofxKinect kinect;
+	uint16_t *raw_depth_pix;
+	uint8_t *rgb_pix;
+	
+	bool init_kinect();
+	bool update_kinect();
+	
+	//	ui
+	
+	bool pressed[512];
+	
+	void init_keys();
+	void update_keys();
+	
+	
+	// app
+	
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void resized(int w, int h);
+	
+	void debug();
+	
+};
